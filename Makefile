934fc720972e214faa941797f4c7c3d791dd1b6a
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-12-20 08:39:17 -0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+WARNINGS:= -Wall
+CFLAGS:= $(WARNINGS) -g 
+
+all: ex1 ex3
+
+clean:
+	rm -f ex1 ex3
+
---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+  int i = 0;
+  int b = 1;
+  int c = i + b;
+  printf("Hello world. %d", c);
+  return 0;
+}
